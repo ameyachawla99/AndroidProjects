3e4af9d10f9a711a187fc82fc57a8f3b194dfbd4
Ameya Chawla <ameyachawla99@gmail.com>
2021-02-20 12:29:05 -0800

    First upload

---
+++ Project-1/app/src/main/java/com/ameyachawla/beeradvisor/FindBeerActivity.java
@@ -0,0 +1,16 @@
+package com.ameyachawla.beeradvisor;
+import java.util.ArrayList;
+import java.util.List;
+public class FindBeerActivity {
+    List<String> getBrands(String color) {
+        List<String> brands = new ArrayList<>();
+        if (color.equals("amber")) {
+            brands.add("Jack Amber");
+            brands.add("Red Moose");
+        } else {
+            brands.add("Jail Pale Ale");
+            brands.add("Gout Stout");
+        }
+        return brands;
+    }
+}
